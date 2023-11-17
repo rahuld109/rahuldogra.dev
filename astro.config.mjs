5f51a1bed6ba9599e7c39a4780e738ce13e8ed0d
Rahul Dogra <rahuldogra1998@gmail.com>
2023-11-17 21:45:09 +0530

    rm: vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     sitemap(),
     partytown({
       config: {
-        forward: ['dataLayer.push', 'va'],
+        forward: ['dataLayer.push'],
       },
     }),
   ],
