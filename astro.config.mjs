4eefd7942d9bf0696c73909953f6704d6905e0c2
Rahul Dogra <rahuldogra1998@gmail.com>
2023-11-17 21:27:19 +0530

    perf: added ga in partytown and misc improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   site: 'https://www.rahuldogra.dev',
   vite: {
     ssr: {
