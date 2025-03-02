12edcc65e08f413d4b7f7504af9889609eb1024a
rahuld109 <rahuldogra1998@gmail.com>
2025-03-02 06:08:26 +0530

    bump: astro version 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,27 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import partytown from '@astrojs/partytown';
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import partytown from "@astrojs/partytown";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
-  site: 'https://www.rahuldogra.dev',
-  vite: {
-    ssr: {
-      external: ['svgo'],
-    },
-  },
+	// Add your production domain here
+	site: "https://rahuldogra.dev",
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+		icon(),
+	],
 });
